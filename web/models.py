781d3030bdbf9f8f710a0eff7cc205269e443900
Luca Ferroni <luca@befair.it>
2024-03-29 23:30:46 +0100

    Updates Action model so that migration can be created

--- web/models.py
+++ web/models.py
@@ -6,11 +6,14 @@ from django.db.models.functions import Greatest
 class Action(models.Model):
 
     name = models.CharField(max_length=128)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    confirmed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    canceled_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    last_updated_by = models.GeneratedField(
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                            related_name="created_actions_set", null=True)
+    confirmed_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                            related_name="confirmed_actions_set", null=True)
+    canceled_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                            related_name="canceled_actions_set", null=True)
+    last_updated_by_id = models.GeneratedField(
                         expression=Greatest("created_by", "confirmed_by", "canceled_by"),
-                        output_field=models.ForeignKey(User, on_delete=models.SET_NULL, null=True),
+                        output_field=models.BigIntegerField(),
                         db_persist=True)
 
