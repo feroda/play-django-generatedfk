296906fa33dce2ff84fd86e9011d21463e2f29bc
Luca Ferroni <luca@befair.it>
2024-03-30 14:38:29 +0100

    Seems to not work in SQLite3, adds PostgreSQL setup in settings

--- generatedfk/settings.py
+++ generatedfk/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'generatedfk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev1',
+        'USER': 'CHANGEME',
+        'PASSWORD': 'CHANGEME',
+        'HOST': 'localhost',
     }
 }
 
