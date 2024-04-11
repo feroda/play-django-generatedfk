45f3aaefe3ec189373f7bbd308e1f3e73b5f07cc
Luca Ferroni <luca@befair.it>
2024-04-11 13:44:32 +0200

    Adds new app fobject with model and tests copied from noopfk to test djanog #35269 comment:7

--- fobject/models.py
+++ fobject/models.py
@@ -2,23 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.functions import Greatest
 
-from .fields import NoopForeignKey
-
 
 class Event(models.Model):
 
     name = models.CharField(max_length=128)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL,
-                            related_name="created_events_set", null=True)
+                            related_name="created_xevents_set", null=True)
     confirmed_by = models.ForeignKey(User, on_delete=models.SET_NULL,
-                            related_name="confirmed_events_set", null=True)
+                            related_name="confirmed_xevents_set", null=True)
     canceled_by = models.ForeignKey(User, on_delete=models.SET_NULL,
-                            related_name="canceled_events_set", null=True)
+                            related_name="canceled_xevents_set", null=True)
     last_updated_by_id = models.GeneratedField(
                         expression=Greatest("created_by", "confirmed_by", "canceled_by"),
                         output_field=models.BigIntegerField(),
                         db_index=True, db_persist=True)
     
-    last_updated_by = NoopForeignKey(User, on_delete=models.SET_NULL,
-                            related_name="last_updated_events_set", null=True)
+    # last_updated_by = NoopForeignKey(User, on_delete=models.SET_NULL,
+    #                         related_name="last_updated_events_set", null=True)
 
---
+++ fobject/tests.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from fobject.models import Event
+
+class EventTestCase(TestCase):
+
+    def setUp(self):
+        self.u = "cielcio"
+        User.objects.create(username=self.u)
+        Event.objects.create(name="releasing Django 6.0", created_by=User.objects.first())
+
+    def test_events_last_updated_by_fk_generated(self):
+        """Events have the same values for created_by and last_updated_by GeneratedForeignKey"""
+        self.assertEqual(
+            Event.objects.first().created_by,
+            Event.objects.first().last_updated_by)
+
+    def test_events_last_updated_by_fk_lookup(self):
+        """GeneratedForeignKey can use the Queryset lookup API"""
+        self.assertEqual(
+            list(Event.objects.filter(created_by__username__startswith=self.u).values()),
+            list(Event.objects.filter(last_updated_by__username__startswith=self.u).values()))
+
+    def test_events_bulk_create(self):
+        """GeneratedForeignKey must support bulk create"""
+        Event.objects.bulk_create([
+                Event(name="First event"),
+                Event(name="Second event", created_by=User.objects.first()),
+            ])
