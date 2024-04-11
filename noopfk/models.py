7675eff7ab8d4def898a6e7814e93adf386c56d7
Luca Ferroni <luca@befair.it>
2024-04-11 13:29:51 +0200

    Removes unused import

--- noopfk/models.py
+++ noopfk/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.functions import Greatest
-from django.db.models.fields.related_descriptors import ForwardManyToOneDescriptor
 
 from .fields import NoopForeignKey
 
